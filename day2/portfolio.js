dc1981b4e8e768b276afa8d83ab8601b44f277c4
katya <katya.gurina@yahoo.com>
2017-03-20 23:54:04 -0400

    test 3

---
+++ day2/portfolio.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+	
+//	$(".project").hover(function(){
+//		$("#project-card-1").slideToggle("slow");
+//		}, function(){
+//		$("#project-card-1").slideToggle("slow");
+//	});	
+
+
+	
+$(".project").hover(function(){
+    $(this).children(".project-card").slideToggle("slow");
+    }, function(){
+    $(this).children(".project-card").slideToggle("slow");
+});		
+
+$('span.nav-btn').click(function(){
+$("ul.nav").slideToggle();
+});
+});
+	
+$(window).resize(function(){
+		$("ul.nav").removeAttr("style");
+	});
+
+	
+	$("ul.nav > li").click(function(){
+		if ($(window).width() < 414){
+		$("ul.nav").slideUp();
+		}	
+	});
+
