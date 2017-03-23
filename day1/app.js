9cbd461fa9f484ad550b4a95d068c764c4d8b5d3
katya <katya.gurina@yahoo.com>
2017-03-22 20:23:38 -0400

    finished layout of the page, fixed links, added footer

---
+++ day1/app.js
@@ -0,0 +1,122 @@
+/*$(document).ready(function() {
+  // Get an array of all element heights
+function evenHeight(){
+	  var elementHeights = $('.service-box').map(function() {
+	    return $(this).height();
+	  }).get();
+
+	  // Math.max takes a variable number of arguments
+	  // `apply` is equivalent to passing each height as an argument
+	  var maxHeight = Math.max.apply(null, elementHeights);
+
+	  // Set each height to the max height
+	  $('.service-box').height(maxHeight);
+	}
+
+	window.onresize = evenHeight;
+
+});
+
+*/
+
+/* Smooth scroll effect on nav element click */
+$(document).ready(function () {
+    $(document).on("scroll", onScroll);
+    
+    //smoothscroll
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+        $(document).off("scroll");
+        
+        $('a').each(function () {
+            $(this).removeClass('active');
+        })
+        $(this).addClass('active');
+      
+        var target = this.hash,
+            menu = target;
+        $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top+2
+        }, 500, 'swing', function () {
+            window.location.hash = target;
+            $(document).on("scroll", onScroll);
+        });
+    });
+});
+
+function onScroll(event){
+    var scrollPos = $(document).scrollTop();
+    $('#main-nav li a').each(function () {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+            $('#main-nav li').removeClass("active");
+            $(this).closest('li').addClass("active");
+        }
+        else{
+            currLink.removeClass("active");
+        }
+    });
+
+
+
+/* mailer function */
+$(function() {
+
+	// Get the form.
+	var form = $('#contact-form');
+
+	// Get the messages div.
+	var formMessages = $('#form-messages');
+
+	// Set up an event listener for the contact form.
+	$(form).submit(function(e) {
+		// Stop the browser from submitting the form.
+		e.preventDefault();
+
+		// Serialize the form data.
+		var formData = $(form).serialize();
+
+		// Submit the form using AJAX.
+		$.ajax({
+			type: 'POST',
+			url: $(form).attr('action'),
+			data: formData
+		})
+		.done(function(response) {
+			// Make sure that the formMessages div has the 'success' class.
+			$(formMessages).removeClass('error');
+			$(formMessages).addClass('success');
+
+			// Set the message text.
+			$(formMessages).text(response);
+
+			// Clear the form.
+			$('#name').val('');
+			$('#email').val('');
+			$('#message').val('');
+		})
+		.fail(function(data) {
+			// Make sure that the formMessages div has the 'error' class.
+			$(formMessages).removeClass('success');
+			$(formMessages).addClass('error');
+
+			// Set the message text.
+			if (data.responseText !== '') {
+				$(formMessages).text(data.responseText);
+			} else {
+				$(formMessages).text('Oops! An error occured and your message could not be sent.');
+			}
+		});
+
+	});
+
+});
+
+}
+
+
+
+
+
